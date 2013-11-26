33fa646440b0b7b71106b8ab46aac9ed751ef7ee
Javi Manzano <javi.manzano.oller@gmail.com>
2013-11-26 23:07:45 +0000

    Add grunt

---
+++ Gruntfile.js
@@ -0,0 +1,46 @@
+module.exports = function(grunt) {
+
+	// Project configuration.
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		sass: {
+			dev: {
+				options: {
+					style: "expanded"
+				},
+				files: {
+					'app/css/styles.css': 'app/scss/python-warrior.scss'
+				}
+			}
+		},
+		watch: {
+			dev: {
+				files: ['app/scss/**/*.scss'],
+				tasks: ['sass:dev', 'notify:css']
+			}
+		},
+		notify: {
+			css: {
+				options: {
+					title: 'Task Complete',
+					message: 'CSS is ready'
+				}
+			}
+		},
+		concurrent: {
+			watch: {
+				tasks: ['watch:dev'],
+				options: {
+					logConcurrentOutput: true
+				}
+			}
+		},
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-concurrent');
+	grunt.loadNpmTasks('grunt-notify');
+
+	grunt.registerTask('default', ['sass:dev', 'concurrent:watch']);
+};
--- README.md
+++ README.md
@@ -5,3 +5,8 @@ This is an attempt to make a game similar to [Ruby Warrior](https://www.bloc.io/
 It won't have so great design or "art", since everything will be done by myself.
 
 The reason I'm building this is I want to experiment with web based code interpreters. For this project I'm using [skulpt](https://github.com/skulpt/skulpt).
+
+## TODOS:
+
+- Use local storage for saving the content of the text editor - save button.
+- Create backbone stuff.
