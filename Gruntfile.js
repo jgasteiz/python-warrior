3f36a92666c8ff32c45ae67fba32ed365f39eb25
Javi Manzano <javi.manzano.oller@gmail.com>
2013-11-27 19:03:13 +0000

    Add a couple of new node packages and improve the grunt workflow

--- Gruntfile.js
+++ Gruntfile.js
@@ -35,12 +35,62 @@ module.exports = function(grunt) {
 				}
 			}
 		},
+		copy: {
+			main: {
+				files: [
+					// jQuery
+					{
+						expand: true,
+						src: [
+							'bower_components/jquery/*.min.js',
+							'bower_components/jquery/*.min.map'
+						],
+						dest: 'app/js/vendor/jquery',
+						flatten: true,
+						filter: 'isFile'
+					},
+					// Underscore
+					{
+						expand: true,
+						src: [
+							'bower_components/underscore/*-min.js',
+							'bower_components/underscore/*-min.map'
+						],
+						dest: 'app/js/vendor/underscore',
+						flatten: true,
+						filter: 'isFile'
+					},
+					// Backbone
+					{
+						expand: true,
+						src: [
+							'bower_components/backbone/*-min.js',
+							'bower_components/backbone/*-min.map'
+						],
+						dest: 'app/js/vendor/backbone',
+						flatten: true,
+						filter: 'isFile'
+					},
+					// Skulpt
+					{
+						expand: true,
+						src: [
+							'bower_components/skulpt/*.min.js'
+						],
+						dest: 'app/js/vendor/skulpt',
+						flatten: true,
+						filter: 'isFile'
+					}
+				]
+			}
+		}
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks('grunt-concurrent');
 	grunt.loadNpmTasks('grunt-notify');
 
-	grunt.registerTask('default', ['sass:dev', 'concurrent:watch']);
+	grunt.registerTask('default', ['copy', 'sass:dev', 'concurrent:watch']);
 };
